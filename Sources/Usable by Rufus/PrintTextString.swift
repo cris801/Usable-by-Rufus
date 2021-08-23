8270d2a5f2757c809aa7a4c260f08f2ae9c2bea5
Cristian Pena B <48634410+cris801@users.noreply.github.com>
2021-08-23 13:37:21 -0500

    first commit

---
+++ Sources/Usable by Rufus/PrintTextString.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by cpenab on 23/08/21.
+//
+
+import Foundation
+
+class PrintTextString {
+    
+    func printxtStr(str: String) {
+        print("TU CADENA CONTIENE: \(str)")
+    }
+}
