3611dd728f005479d2a4bcf5c68ac90654865880
Cristian Pena B <48634410+cris801@users.noreply.github.com>
2021-08-23 18:19:04 -0500

    mejorando

--- Sources/Usable by Rufus/Usable_by_Rufus.swift
+++ Sources/Usable by Rufus/Usable_by_Rufus.swift
@@ -1,15 +1,16 @@
 public let Rufus = PrintTextString.default
+let version = "0.0.1"
 
-public struct Usable_by_Rufus {
-    var text = "Hello, World!"
-    
-    
-    
-    public init() {
-        
-    }
-    
-    public func printxtStr(str: String) {
-        print("TU CADENA CONTIENE: \(str)")
-    }
-}
+//public struct Usable_by_Rufus {
+//    var text = "Hello, World!"
+//
+//
+//
+//    public init() {
+//
+//    }
+//
+//    public func printxtStr(str: String) {
+//        print("TU CADENA CONTIENE: \(str)")
+//    }
+//}
