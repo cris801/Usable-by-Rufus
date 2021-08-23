7aec4770741551be537b868d9bcd96d3f9ed5d2e
Cristian Pena B <48634410+cris801@users.noreply.github.com>
2021-08-23 17:07:18 -0500

    agregando el init

--- Sources/Usable by Rufus/Usable_by_Rufus.swift
+++ Sources/Usable by Rufus/Usable_by_Rufus.swift
@@ -1,6 +1,10 @@
 public struct Usable_by_Rufus {
     var text = "Hello, World!"
     
+    public init() {
+        
+    }
+    
     public func printxtStr(str: String) {
         print("TU CADENA CONTIENE: \(str)")
     }
